13f792baa70a713bf467ed997ce7a5d31a459391
Atsamaz Gudiev <officiallacc@gmail.com>
2023-05-12 16:38:23 +0300

    first commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,19 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean1 =  (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =  (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("HelloWorld objects comparison: " + (bean1 == bean2));
+
+
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Cat objects comparison: " + (catBean1 == catBean2));
+
+
+        System.out.println(bean1.getMessage());
     }
 }
